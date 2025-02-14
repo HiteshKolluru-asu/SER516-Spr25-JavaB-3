27b6e443773e319384969c55cb4b5ba8ab5d974a
falaqal <falaqal@asu.edu>
2025-02-13 20:53:43 -0700

    Added Efferent Coupling UI Task #34

--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,12 @@ function showResult(resultId) {
     document.getElementById(resultId).style.display = 'block';
 }
 
+// Afferent Coupling Button Click
 document.getElementById('submitAfferent').addEventListener('click', function() {
     showResult('resultAfferent');
+});
+
+// Efferent Coupling Button Click
+document.getElementById('submitEfferent').addEventListener('click', function() {
+    showResult('resultEfferent');
 });
