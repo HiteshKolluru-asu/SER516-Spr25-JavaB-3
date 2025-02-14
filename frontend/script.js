bf504de0f448c83422acc5b884d2cef144e0b85b
falaqal <falaqal@asu.edu>
2025-02-13 23:31:01 -0700

    Added Afferent Coupling UI (Task #16)

--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,7 @@ function showResult(resultId) {
     document.getElementById(resultId).style.display = 'block';
 }
 
+
 document.getElementById('submitAfferent').addEventListener('click', function() {
     showResult('resultAfferent');
 });
