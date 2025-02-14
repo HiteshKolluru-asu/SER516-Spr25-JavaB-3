94dab95f7f5efe20b9d96344a1b9847e06dd1bd2
HiteshKolluru-asu <hkolluru@asu.edu>
2025-02-14 00:03:09 -0700

    Created Basic API
    Basic API that handles default value, random number and requested Number. and Incase error.

---
+++ DefectDensity-API/src/main/java/com/defectdensityapi/Model/RequestedNumber.java
@@ -0,0 +1,19 @@
+package com.defectdensityapi.Model;
+
+public class RequestedNumber {
+    private int number;
+
+    public RequestedNumber(int number) {
+
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+}
---
+++ DefectDensity-API/src/test/java/com/defectdensityapi/DefectDensityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defectdensityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefectDensityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
