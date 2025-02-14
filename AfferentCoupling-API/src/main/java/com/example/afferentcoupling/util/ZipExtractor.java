b2bf361d8bc4a7c97664ae3890955c942f05c841
thrupthihm <thosahal@asu.edu>
2025-02-13 22:47:10 -0700

    Updated the logic for calculating afferent coupling
    1. Updated logic again to get proper results.

--- AfferentCoupling-API/src/main/java/com/example/afferentcoupling/util/ZipExtractor.java
+++ AfferentCoupling-API/src/main/java/com/example/afferentcoupling/util/ZipExtractor.java
@@ -4,49 +4,28 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class ZipExtractor {
-
-    public static Set<String> extractJavaFiles(MultipartFile file) throws IOException {
-        Set<String> javaFiles = new HashSet<>();
+    public static List<String> extractJavaFiles(MultipartFile file) throws IOException {
+        List<String> javaFiles = new ArrayList<>();
         try (ZipInputStream zis = new ZipInputStream(file.getInputStream())) {
             ZipEntry entry;
             while ((entry = zis.getNextEntry()) != null) {
                 if (entry.getName().endsWith(".java")) {
-                    javaFiles.add(formatClassName(entry.getName()));
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(zis));
+                    StringBuilder fileContent = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        fileContent.append(line).append("\n");
+                    }
+                    javaFiles.add(fileContent.toString());
                 }
             }
         }
         return javaFiles;
-    }
-
-    public static String readJavaFile(MultipartFile file, String targetClass) throws IOException {
-        StringBuilder content = new StringBuilder();
-        try (ZipInputStream zis = new ZipInputStream(file.getInputStream())) {
-            ZipEntry entry;
-            while ((entry = zis.getNextEntry()) != null) {
-                String className = formatClassName(entry.getName());
-                if (className.equals(targetClass)) {
-                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(zis))) {
-                        String line;
-                        while ((line = reader.readLine()) != null) {
-                            content.append(line).append("\n");
-                        }
-                    }
-                    break;
-                }
-            }
-        }
-        return content.toString();
-    }
-
-    private static String formatClassName(String filePath) {
-        filePath = filePath.replace("/", ".").replace(".java", "");
-        int index = filePath.indexOf("com.example");
-        return (index != -1) ? filePath.substring(index) : filePath;
     }
 }
